f4d81f0e8dfc58f6fca8e9e3ac272bb30470cc6d
Naoki Egami <naoki-egami@users.noreply.github.com>
2020-12-05 16:12:09 -0500

    up

--- example/sign_toy_example.R
+++ example/sign_toy_example.R
@@ -1,3 +1,9 @@
-pv <- sample(x = c(0.001, 0.02, 0.04, 0.05, 0.1, 0.2, 0.003), size = 8, replace = TRUE)
+set.seed(1234)
 
-pct(pv)
+check <- c()
+for(i in 1:1000){
+  pv <- sample(x = c(0.001, 0.02, 0.04, 0.05, 0.1, 0.2, 0.003), size = 8, replace = TRUE)
+  res <- pct(pv)
+  check[i] <- all(pv[res$h_num] == sort(pv))
+}
+all(check)
